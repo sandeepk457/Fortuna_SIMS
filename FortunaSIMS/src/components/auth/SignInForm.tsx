cf3a99a1b041d50120145542662de27828d867b8
Sandeep <sandeepinv.sd2020@gmail.com>
2026-07-20 13:39:34 +0530

    Configure API URL for cloud deployment

--- FortunaSIMS/src/components/auth/SignInForm.tsx
+++ FortunaSIMS/src/components/auth/SignInForm.tsx
@@ -36,7 +36,10 @@ export default function SignInPage() {
 
   try {
 
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+   const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+
+const res = await fetch(`${API_URL}/api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
