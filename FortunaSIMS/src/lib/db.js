5c4f4531d637db08a5892070a9bd7a6b79e64362
Sandeep <sandeepinv.sd2020@gmail.com>
2026-07-22 10:49:51 +0530

    Add production database fallback support

--- FortunaSIMS/src/lib/db.js
+++ FortunaSIMS/src/lib/db.js
@@ -2,12 +2,19 @@ import pkg from "pg";
 
 const { Pool } = pkg;
 
-const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "Tanvitha@0506",
-  database: "fortuna_sims_db",
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      host: "localhost",
+      port: 5432,
+      user: "postgres",
+      password: "Tanvitha@0506",
+      database: "fortuna_sims_db",
+    });
 
 export { pool };
