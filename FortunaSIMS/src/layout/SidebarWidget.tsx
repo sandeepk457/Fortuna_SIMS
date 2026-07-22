b5540fc39bd117275defabed781c7c78fbc4506d
Sandeep <sandeepinv.sd2020@gmail.com>
2026-07-22 10:04:34 +0530

    website updated in menubar

--- FortunaSIMS/src/layout/SidebarWidget.tsx
+++ FortunaSIMS/src/layout/SidebarWidget.tsx
@@ -13,7 +13,7 @@ export default function SidebarWidget() {
         "A Product From Fortuna Global Supply Chain Systems"
       </p>
       <a
-        href="https://fortunaglobalsupplychain.com/index.html"
+        href="https://fortunaglobalsupplychain.com/"
         target="_blank"
         rel="nofollow"
         className="flex items-center justify-center p-3 font-medium text-white rounded-lg bg-brand-500 text-theme-sm hover:bg-brand-600"
