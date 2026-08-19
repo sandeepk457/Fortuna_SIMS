90f113482077a9a9b9361ec905959735d762dc3d
Sandeep <sandeepinv.sd2020@gmail.com>
2026-08-20 00:17:44 +0530

    Update dashboard background theme

--- FortunaSIMS/src/app/layout.tsx
+++ FortunaSIMS/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${outfit.className} dark:bg-gray-900`}>
+      <body className={`${outfit.className} bg-[#F4F7FA] dark:bg-gray-900`}>
         <ThemeProvider>
           <SidebarProvider>{children}</SidebarProvider>
         </ThemeProvider>
